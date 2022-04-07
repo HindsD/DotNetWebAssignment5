1e81d201e5d7934937d7ff8749b3287019362f7b
Danny <dhinds423@gmail.com>
2022-04-07 10:17:43 -0500

    Final with Lynn and Danny

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
 using System.Linq;
@@ -6,14 +7,10 @@ namespace Northwind.Controllers
 {
     public class ProductController : Controller
     {
-        // this controller depends on the BloggingRepository
         private NorthwindContext _northwindContext;
         public ProductController(NorthwindContext db) => _northwindContext = db;
         public ActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
-        public IActionResult Index(int id) => View(new ProductViewModel
-        {
-            category = _northwindContext.Categories.FirstOrDefault(c => c.CategoryId == id),
-            Product = _northwindContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false)
-        });
+        public IActionResult Index(int id) => View(_northwindContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+        public ActionResult Discount() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Northwind.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Northwind
 {
@@ -31,6 +32,8 @@ namespace Northwind
         {
             // this is where we use the config info for our connection string
             services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration["Data:Northwind:ConnectionString"]));
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["Data:AppIdentity:ConnectionString"]));
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
             services.AddControllersWithViews();
         }
 
@@ -44,6 +47,8 @@ namespace Northwind
 
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
